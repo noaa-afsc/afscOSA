47b015694be23058620b864ebf6628deb0fe058a
Jane Sullivan <jane.sullivan@noaa.gov>
2026-07-27 12:40:48 -0800

    old code

--- R/run_osa.R
+++ R/run_osa.R
@@ -144,7 +144,6 @@ run_osa <- function(obs, exp, N, fleet, index, years,
   }
   pearson <- (o-N*p)/sqrt(V)
   # long format dataframe for residuals
-  # FLAG - check this change:
   # dimnames(mat) <- list(year=years, index=index[-1])
   dimnames(pearson) <- list(year=years, index=index)
   pearson <- reshape2::melt(pearson, value.name='resid') |>
